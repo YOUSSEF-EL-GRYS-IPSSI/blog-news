60837acb6278ed327e3628a19d12d29da50c6b26
Youssef <elgrysyoussef78@gmail.com>
2023-10-17 17:42:30 +0200

    mise en place js pour la confirmation de suppression article

--- show-article.php
+++ show-article.php
@@ -43,7 +43,9 @@ if (!$id) {
                 <div class="separateur"></div>
                 <p class="article-content"><?= $article['content'] ?></p>
                 <div class="action">
-                    <a class="btn btn-secondary" href="/delete-article.php?id=<?= $article['id']  ?>" type="button">Supprimé</a>
+                    <!-- Mettez l'URL de suppression directement dans l'attribut "href" du lien -->
+                    <a class="btn btn-secondary" id="deleteButton" data-id="<?= $article['id'] ?>" href="/delete-article.php?id=<?= $article['id'] ?>" type="button">Supprimé</a>
+
                     <a class="btn btn-primary" href="/form-article.php?id=<?= $article['id'] ?>">Editer l'article</a>
                 </div>
             </div>
@@ -51,6 +53,43 @@ if (!$id) {
         </div>
         <?php require_once 'includes/footer.php' ?>
     </div>
+
+
+    <div id="deleteModal" class="delete-modal">
+        <div class="modal-content">
+            <h2>Confirmation</h2>
+            <p>Êtes-vous sûr de vouloir supprimer votre article?</p>
+            <button id="confirmDelete">Confirmer</button>
+            <button id="cancelDelete">Annuler</button>
+        </div>
+    </div>
+
+
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const deleteButton = document.getElementById('deleteButton');
+            const deleteModal = document.getElementById('deleteModal');
+            const confirmDelete = document.getElementById('confirmDelete');
+            const cancelDelete = document.getElementById('cancelDelete');
+
+            deleteButton.addEventListener('click', function(event) {
+                event.preventDefault();
+                deleteModal.style.display = 'block';
+            });
+
+            confirmDelete.addEventListener('click', function() {
+                const articleId = deleteButton.getAttribute('data-id');
+                window.location.href = '/delete-article.php?id=' + articleId;
+            });
+
+            cancelDelete.addEventListener('click', function() {
+                deleteModal.style.display = 'none';
+            });
+        });
+    </script>
 </body>
+
+
 
 </html>
